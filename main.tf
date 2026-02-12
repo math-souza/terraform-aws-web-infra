484db6acc79b93cc04aa048b56effa364ba29852
Matheus <46329824+math-souza@users.noreply.github.com>
2026-02-11 23:11:04 -0300

    Add initial AWS provider and EC2 instance configuration

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Configuracao do AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Configuracao da VPC
+
+# Configuracao da EC2
+
+# AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+# Instancia
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-instance"
+  }
+}
+
+https://339712813620.signin.aws.amazon.com/console
