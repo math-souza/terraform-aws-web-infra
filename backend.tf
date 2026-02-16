cc841c852e38c60c89447ede6a1a3cf1b9ae41e0
Matheus <46329824+math-souza@users.noreply.github.com>
2026-02-16 17:15:59 -0300

    Update S3 backend bucket name in Terraform config

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 backend "s3" {
-  bucket = "webserver-terraform-state" 
+  bucket = "webserver-project-matheus" 
   key    = "webserver/terraform.tfstate"   
   region = "us-east-1"
   encrypt = true
 }
+
