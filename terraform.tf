cf3030b4b70dceae44a3bf8ab3e33a9247f211f8
Matheus <46329824+math-souza@users.noreply.github.com>
2026-02-16 17:50:05 -0300

    Configure S3 backend for Terraform
    Added S3 backend configuration for Terraform state management.

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "webserver-project-matheus" 
+    key    = "webserver/terraform.tfstate"   
+    region = "us-east-1"
+    encrypt = true
+  }
+
 }
+
