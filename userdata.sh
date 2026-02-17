8b8d831d5e513d8ad0b5183f15bb4def3cb86c2e
Matheus <46329824+math-souza@users.noreply.github.com>
2026-02-16 23:36:54 -0300

    Switch from Apache to Nginx and update userdata.sh
    Replaced Apache with Nginx and added S3 file retrieval.

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,17 @@
+user_data = <<-EOF
 #!/bin/bash
 yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "Servidor Web funcionando" > /var/www/html/index.html
+yum install -y nginx aws-cli
+
+systemctl start nginx
+systemctl enable nginx
+
+# limpar pasta padrão
+rm -rf /usr/share/nginx/html/*
+
+# baixar arquivos do S3
+aws s3 cp s3://${aws_s3_bucket.website-project-matheus.bucket}/ /usr/share/nginx/html/ --recursive
+
+# reiniciar nginx
+systemctl restart nginx
+EOF
