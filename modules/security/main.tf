f6b5553981f789272b0a84c7bf524e73527ed0c2
Matheus <46329824+math-souza@users.noreply.github.com>
2026-03-07 21:04:04 -0300

    Create security groups for ALB and EC2 web server
    Added security groups for ALB and EC2 web server with ingress and egress rules.

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,28 +1,61 @@
-resource "aws_security_group" "alb_sg" {
-  vpc_id = var.vpc_id
+# Security Group ALB
+resource "aws_security_group" "alb-sg-webserver" {
+  vpc_id = aws_vpc.web-server-vpc.id
+  description = "SG para o Application Load Balancer"
+  name = "alb-sg-webserver"
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    description = "HTTP para Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
+    description = "HTTPS para Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Permitir toda saida"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg-webserver"
   }
 }
 
-resource "aws_security_group" "ec2_sg" {
-  vpc_id = var.vpc_id
+# Security Group EC2
+resource "aws_security_group" "ec2-sg-webserver" {
+  name = "ec2-sg-webserver"
+  description = "SG para Instancias EC2 Web Server"
+  vpc_id = aws_vpc.web-server-vpc.id
 
   ingress {
+    description = "HTTP para ALB"
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    security_groups = [aws_security_group.alb-sg-webserver.id]
+  }
+
+  egress {
+    description = "Permitir toda saida"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg-webserver"
   }
 }
