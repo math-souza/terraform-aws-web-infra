a7e2019d82847604ff19ab580107967d4fba030b
matheus <matheus@email.com>
2026-03-07 23:52:43 -0300

    Update varibles.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,5 +1,14 @@
 variable "bucket_name" {
   type = string
-  default = website-project-matheus
+  default = "website-project-matheus"
 }
 
+variable "ami_id" {
+  type = string
+  default = "ami-0c1fe732b5494dc14"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
