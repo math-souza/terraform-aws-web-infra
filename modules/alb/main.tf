fe3d4d74dc5c83a27bb8e848101c9b9996bca5c5
matheus <matheus@email.com>
2026-03-07 23:58:04 -0300

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,6 +1,6 @@
 # ALB
 resource "aws_lb" "web-server-alb" {
-  name               = "web-server-alb"
+  name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb-sg-webserver.id]
@@ -15,7 +15,7 @@ resource "aws_lb" "web-server-alb" {
 
 # Target Group
 resource "aws_lb_target_group" "webserver-tg" {
-  name     = "webserver-tg"
+  name     = var.tg_name
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.web-server-vpc.id
@@ -31,7 +31,7 @@ resource "aws_lb_target_group" "webserver-tg" {
     unhealthy_threshold = 2
   }
 
-  deregistration_delay = 30
+  deregistration_delay = var.deregistration_delay
 
   tags = {
     Name = "web-target-group"
