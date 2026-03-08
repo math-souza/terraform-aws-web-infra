e006fb71a6a4fdcf76ca48ee232cbf4b6db6b0a7
Matheus <46329824+math-souza@users.noreply.github.com>
2026-03-07 21:02:21 -0300

    Update network outputs for webserver configuration

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,21 +1,29 @@
 output "vpc_id" {
-  value = aws_vpc.webserver_vpc.id
+  value = aws_vpc.web-server-vpc.id
 }
 
 output "public_subnets" {
   value = [
-    aws_subnet.public_a.id,
-    aws_subnet.public_b.id
+    aws_subnet.pub-sub-1a-webserver.id,
+    aws_subnet.pub-sub-1b-webserver.id
   ]
 }
 
 output "private_subnets" {
   value = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
+    aws_subnet.priv-sub-1a-webserver.id,
+    aws_subnet.priv-sub-1b-webserver.id
   ]
 }
 
 output "private_route_table_id" {
-  value = aws_route_table.priv.id
+  value = aws_route_table.priv-rtb-webserver.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.pub-rtb-webserver.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.webserver-nat.id
 }
