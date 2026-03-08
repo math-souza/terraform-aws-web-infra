04e759fc4fd6bceb9d96ef69d918dd62c3ac713e
matheus <matheus@email.com>
2026-03-08 00:08:08 -0300

    Update Variables

--- modules/endpoints/main.tf
+++ modules/endpoints/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc_endpoint" "webserver-s3-endpoint" {
   vpc_id            = aws_vpc.web-server-vpc.id
   service_name      = "com.amazonaws.us-east-1.s3"
-  vpc_endpoint_type = "Gateway"
+  vpc_endpoint_type = var.endpoint_type
 
   route_table_ids = [
     aws_route_table.priv-rtb-webserver.id
---
+++ modules/endpoints/variables.tf
@@ -0,0 +1,4 @@
+variable "endpoint_type" {
+	type = string
+	default = "Gateway"
+}
