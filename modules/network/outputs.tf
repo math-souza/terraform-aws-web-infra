aa5c7386e2715e71fd610f4341354b6835712a71
matheus <matheus@email.com>
2026-03-08 21:56:57 -0300

    update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,18 +2,20 @@ output "vpc_id" {
   value = aws_vpc.web-server-vpc.id
 }
 
-output "public_subnets" {
-  value = [
-    aws_subnet.pub-sub-1a-webserver.id,
-    aws_subnet.pub-sub-1b-webserver.id
-  ]
+output "pub_sub_1a" {
+  value = aws_subnet.pub-sub-1a-webserver.id
 }
 
-output "private_subnets" {
-  value = [
-    aws_subnet.priv-sub-1a-webserver.id,
-    aws_subnet.priv-sub-1b-webserver.id
-  ]
+output "pub_sub_1b" {
+  value = aws_subnet.pub-sub-1b-webserver.id
+}
+
+output "priv_sub_1a" {
+  value = aws_subnet.priv-sub-1a-webserver.id
+}
+
+output "priv_sub_1b" {
+  value = aws_subnet.priv-sub-1b-webserver.id
 }
 
 output "private_route_table_id" {
