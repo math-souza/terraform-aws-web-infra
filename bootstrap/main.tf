dae7b1cf4973769407035b5f4335c76087edc221
Matheus <46329824+math-souza@users.noreply.github.com>
2026-06-25 17:51:15 -0300

    Refactor Terraform configuration for AWS resources

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -11,6 +21,27 @@ resource "aws_s3_bucket" "bkt-tfstate-webserver" {
   }
 }
 
+# BLOQUEANDO ACESSO PUBLICO AO BUCKET
+resource "aws_s3_bucket_public_access_block" "tfstate_public_access_block" {
+  bucket = aws_s3_bucket.bkt-tfstate-webserver.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# DEFININDO A CRIPTOGRAFIA DO BUCKET
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_criptografia" {
+  bucket = aws_s3_bucket.bkt-tfstate-webserver.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
 # HABILITANDO VERSIONAMENTO
 resource "aws_s3_bucket_versioning" "tfstate-versioning" {
   bucket = aws_s3_bucket.bkt-tfstate-webserver.id
@@ -20,12 +51,24 @@ resource "aws_s3_bucket_versioning" "tfstate-versioning" {
   }
 }
 
-# CRIANDO A "PASTA"
-resource "aws_s3_object" "tfstate-folder" {
-  bucket = aws_s3_bucket.bkt-tfstate-webserver.id
-  key    = "prod/"
-  content = ""
+# LIFECYCLE RULE
+resource "aws_s3_bucket_lifecycle_configuration" "versionamento-bucket-config" {
+  # Deve ter o versionamento do bucket habilitado previamente
+  depends_on = [aws_s3_bucket_versioning.tfstate-versioning]
+
+  bucket = aws_s3_bucket.bkt-tfstate-webserver.bucket
+
+  rule {
+    id = "prod"
+
+    expiration {
+      days = 15
+    }
+
+    status = "Enabled"
+  }
 }
+
 
 # CRIANDO DYNAMODB LOCKTABLE
 resource "aws_dynamodb_table" "terraform_lock" {
